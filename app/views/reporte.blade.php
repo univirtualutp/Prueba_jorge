761487a6641202e4bd144be6eb3c916257aad5e3
Jorge Suarez <jorgesuarezch@gmail.com>
2014-02-17 17:57:35 -0500

    Fix

--- app/views/reporte.blade.php
+++ app/views/reporte.blade.php
@@ -6,7 +6,7 @@
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js" ></script>
-	{{ HTML::script('assets/validate/validate.js'); }}
+	{{ HTML::script('assets/validate/validate.min.js'); }}
 </head>
 <body>
 
