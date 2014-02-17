fe59590b7ba7349637b8a7ca39a43c43ad774b0f
apache <soporteunivirtual@utp.edu.co>
2014-02-17 14:54:34 -0500

    Controlador de reportes

---
+++ app/controllers/ReporteController.php
@@ -0,0 +1,26 @@
+<?php
+
+class ReporteController extends \BaseController {
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+}
